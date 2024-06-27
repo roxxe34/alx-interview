7ce9adad10e0743fabde8b1c8b735a4ecee871aa
Hamza Farissi <hamzafarissi7@gmail.com>
2024-06-27 22:46:57 +0100

    Refactor pascal_triangle function and add README.md

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,12 +2,12 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangl = [[1]]
+    triangle = [[1]]
     for i in range(1, n):
         row = [1]
         for j in range(1, i):
-            row.append(triangl[i-1][j-1] + triangl[i-1][j])
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
         row.append(1)
-        triangl.append(row)
+        triangle.append(row)
 
-    return triangl
+    return triangle
