c3f7b2d675ac9df896459fa4a8c23f35e2b11779
roxxe34 <hamzafarissi7@gmail.com>
2024-08-28 11:16:13 +0100

    feat: Add island_perimeter function to calculate the perimeter of an island

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Island Perimeter
+"""
+
+def island_perimeter(grid):
+    """
+     returns the perimeter of the island
+    """
+    area = 0
+    for row in grid + list(map(list, zip(*grid))):
+        for i1, i2 in zip([0] + row, row + [0]):
+            area += int(i1 != i2)
+    return area
